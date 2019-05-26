365897525a0285782bab6d1e4d92ee453b81468b
LFZ1994 <lfz1994@qq.com>
2019-05-26 09:52:17 +0800

    first commit

---
+++ iiiroboticslidar2_ros/script/initenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo  'KERNEL=="ttyUSB*", ATTRS{idVendor}=="10c4", ATTRS{idProduct}=="ea60", MODE:="0666", GROUP:="dialout",  SYMLINK+="iiilidar"' >/etc/udev/rules.d/ydlidar.rules
+service udev reload
+sleep 2
+service udev restart
